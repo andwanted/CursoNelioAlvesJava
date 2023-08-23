1ccd09de8e667eb82b755496c461b9d5ac4ebe3e
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-08-23 15:41:58 -0300

    adicionado segundo exemplo secao-19-set

--- workspace/ws-curso-nelio-alves/secao-19-Set/src/application/Program.java
+++ workspace/ws-curso-nelio-alves/secao-19-Set/src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,38 +8,20 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		// TODO Auto-generated method stub
-		Set<String> set = new HashSet<>();
-		set.add("Notebook");
-		set.add("TV");
-		set.add("Tablet");
-		// System.out.println(set.contains("Notebook"));
-		set.remove("TV");
-		set.removeIf(x -> x.charAt(0) == 'N');
-		System.out.println("HashSet");
-		for (String p : set) {
-			System.out.println(p);
-		}
-		System.out.println();
-
-		Set<String> set2 = new TreeSet<>();
-		set2.add("Tv");
-		set2.add("Notebook");
-		set2.add("Tablet");
-		System.out.println("TreeSet");
-		for (String p : set2) {
-			System.out.println(p);
-		}
-		System.out.println();
-		
-		Set<String> set3 = new LinkedHashSet<>();
-		set3.add("TV");
-		set3.add("Notebook");
-		set3.add("Tablet");
-		System.out.println("LinkedHashSe");
-		for (String p : set3) {
-			System.out.println(p);
-		}
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+		// union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
+		// intersection
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
+		// difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 	}
 
 }
