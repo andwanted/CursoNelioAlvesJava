437a32c62441a08987abba556037fb9724f3b202
Anderson Leandro <andersonleandrooo@hotmail.com>
2023-09-22 07:48:34 -0300

    inserção simples com preparedStatement + return generated keys 1

--- workspace/ws-curso-nelio-alves/secao-21-introducao-jdbc-inserindo-dados/src/application/Program.java
+++ workspace/ws-curso-nelio-alves/secao-21-introducao-jdbc-inserindo-dados/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -23,15 +25,26 @@ public class Program {
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES "
-					+ "(?,?,?,?,?)");
+					+ "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
-			st.setString(1, "Anderson Leandro");
-			st.setString(2, "anderson@hotmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("18/04/1993").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
+			st.setString(1, "Leandro");
+			st.setString(2, "leandro@hotmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("17/04/1993").getTime()));
+			st.setDouble(4, 4000.0);
+			st.setInt(5, 3);
 			int rowsAffected = st.executeUpdate();
-			System.out.println("Done !! Rows affected " + rowsAffected);
+
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys(); //retorna um resultSet
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done Id = " + id);
+				}
+			}else {
+				System.out.println("No rown affected!");
+			}
+		
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}catch(ParseException e) {
